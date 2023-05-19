7e8e042e4606ecac5297c50b84c5fdd81f5e0ae4
fernanda-dionello-poatek <fernanda.dionello@poatek.com>
2023-05-19 11:08:44 -0300

    feat: add checkout route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const loginRoute = require("./routes/login_route");
 const usersRoutes = require("./routes/users_routes");
 const productsRoutes = require("./routes/products_routes");
 const ordersRoutes = require("./routes/orders_routes");
+const checkoutRoutes = require("./routes/checkout_routes");
 const users_controller = require("./controllers/users_controller");
 
 app.use(cors());
@@ -20,6 +21,7 @@ app.use(users_controller.tokenValidation);
 app.use("/users", usersRoutes);
 app.use("/products", productsRoutes);
 app.use("/orders", ordersRoutes);
+app.use("/checkout", checkoutRoutes);
 
 app.listen(port, () => {
   console.log("listening in port", port);
